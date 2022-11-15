1a8de4824d669fe020c4e8efd284a8bea8864a74
Treasure Ajefu <treasureajefu@gmail.com>
2022-11-15 14:47:04 +0100

    finished working on the modal

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -1,7 +1,23 @@
+import metamask from '../assets/images/metamask-logo.svg';
+import WalletConnect from '../assets/images/walletconnect-logo.svg';
+import arrow from '../assets/icons/right-arrow.svg';
 function ConnectWallet() {
     return ( 
-        <div>
-            <h1>ConnectWallet</h1>
+        <div className="font-['Red_Rose']">
+            <header className="border-b border-slate-300 p-5 font-medium text-lg">Connect Wallet</header>
+            <main className="p-5">
+                <p className="mb-2 text-gray-800">Choose your preferred wallet:</p>
+                <button className="transition-all rounded-lg px-4 py-2 hover:bg-slate-50 border border-slate-300 mb-2 flex items-center justify-between w-full">
+                    <img className="w-9" src={metamask} alt="" />
+                    <span className="font-['Sora'] text-sm font-medium w-full text-left ml-3">Metamask </span>
+                    <img className="w-2" src={arrow} alt="" />
+                </button>
+                <button className="transition-all rounded-lg px-4 py-2 hover:bg-slate-50 border border-slate-300 mb-2 flex items-center justify-between w-full">
+                    <img className="w-9" src={WalletConnect} alt="" /> 
+                    <span className="font-['Sora'] text-sm font-medium w-full text-left ml-3">WalletConnect</span>
+                    <img className="w-2" src={arrow} alt="" />
+                </button>
+            </main>
         </div>
      );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,59 @@
-import { Link } from 'react-router-dom';
-import Logo from '../assets/images/logo-gradient.svg'
+import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo-gradient.svg";
+import close from "../assets/icons/close.svg";
+import React, { useState } from "react";
+import Modal from "react-modal";
+import ConnectWallet from "./ConnectWallet";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    borderRadius: "12px",
+    padding: '0',
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "white",
+    width: 400,
+  },
+};
+
 function Navbar() {
-    return ( 
-        <header className="w-5/6 h-[15vh] mx-auto flex justify-between items-center">
-            <img src={Logo} className="w-[10em] sm:w-[11em] md:w-[13em]" alt="" />
-            <div className="md:flex justify-between w-full hidden">
-                <nav className="w-fit mx-auto sm:space-x-3 md:space-x-5 lg:space-x-9">
-                    <Link>Home</Link>
-                    <Link>Place to stay</Link>
-                    <span>NFTs</span>
-                    <span>Community</span>
-                </nav>
-                <button>
-                    <Link className="bg-[#A02279] text-white px-2 sm:px-3 md:px-5 py-2 md:py-3 rounded text-sm">Connect wallet</Link>
-                </button>
-            </div>
-        </header>
-     );
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <header className="w-5/6 h-[15vh] mx-auto flex justify-between items-center">
+      <img src={Logo} className="w-[10em] sm:w-[11em] md:w-[13em]" alt="" />
+      <div className="md:flex justify-between w-full hidden">
+        <nav className="w-fit mx-auto my-auto sm:space-x-3 md:space-x-5 lg:space-x-9">
+          <Link>Home</Link>
+          <Link>Place to stay</Link>
+          <span>NFTs</span>
+          <span>Community</span>
+        </nav>
+        <button
+          onClick={() => setModalOpen(true)}
+          className="bg-[#A02279] text-white px-2 sm:px-3 md:px-5 py-2 md:py-3 rounded text-sm"
+        >
+          Connect wallet
+        </button>
+        <Modal
+          isOpen={modalOpen}
+          onRequestClose={() => setModalOpen(false)}
+          style={customStyles}
+        >
+            <ConnectWallet />
+            <button 
+                className="absolute top-6 right-6 w-5"
+                onClick={() => setModalOpen(false)}>
+                <img src={close} />
+            </button>
+        </Modal>
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
