e0c4ce7c98b60466547bc72429ebd4feb222fa13
Treasure Ajefu <treasureajefu@gmail.com>
2022-11-16 09:28:54 +0100

    navbar responsiveness done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,18 +26,43 @@ function Navbar() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
 
   return (
-    <header className="w-5/6 h-[15vh] mx-auto flex justify-between items-center">
-      <img src={Logo} className="w-[10em] sm:w-[11em] md:w-[13em]" alt="" />
-      <div className="md:flex justify-between w-full hidden">
-        <nav className="w-fit mx-auto my-auto sm:space-x-3 md:space-x-5 lg:space-x-9">
-          <Link>Home</Link>
-          <Link to="/placeToStay">Place to stay</Link>
+    <header className=" sm:relative w-5/6 h-[15vh] mx-auto flex justify-between items-center">
+      <img src={Logo}
+           className="w-[10em] sm:w-[11em] md:w-[13em]" alt="" />
+      <div 
+        className={
+          isNavExpanded ? "expanded h-screen fixed top-0 left-0 space-y-4 w-screen right-0  bg-white flex flex-col justify-center items-center" : "md:flex justify-between w-full hidden navigation-menu"
+        }
+        >
+        <img 
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+          className="sm:hidden absolute top-24 right-14 w-8 hover:cursor-pointer hover:w-9 transition-all" 
+          src={close} alt="" />
+        <nav className={
+          isNavExpanded ? " flex flex-col justify-center items-center" : "my-auto mx-auto space-x-4 space-y-4 sm:my-auto sm:space-x-3 md:space-x-5 lg:space-x-9"}>
+          <Link 
+            to="/" 
+            onClick={() => {
+              setIsNavExpanded(!isNavExpanded);
+            }} 
+          >
+            Home
+          </Link>
+          <Link 
+            to="/placeToStay"
+            onClick={() => {
+              setIsNavExpanded(!isNavExpanded);
+            }}
+          >
+            Place to stay</Link>
           <span>NFTs</span>
           <span>Community</span>
         </nav>
         <button
           onClick={() => setModalOpen(true)}
-          className="bg-[#A02279] text-white px-2 sm:px-3 md:px-5 py-2 md:py-3 rounded text-sm"
+          className="bg-[#A02279] h-fit text-white px-2 sm:px-3 md:px-5 py-2 md:py-3 rounded text-sm"
         >
           Connect wallet
         </button>
@@ -54,7 +79,10 @@ function Navbar() {
             </button>
         </Modal>
       </div>
-      <img src={menu} alt="" />
+      <img onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }} src={menu} alt="" 
+          className="md:hidden"/>
     </header>
   );
 }
